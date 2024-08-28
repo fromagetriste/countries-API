5f38a39441f4d73355a4209515638c18d381ddc5
fromagetriste __ <fromagetriste@gmail.com>
2024-08-28 15:49:03 -0600

    first and last commit - small project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+      <h3>React World</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+        impedit sunt, ratione modi optio voluptatem rem, repellat dolores
+        laborum cupiditate tenetur non labore dicta aliquid blanditiis, natus
+        quae nihil inventore nesciunt eaque atque. Aut doloribus suscipit a
+        quidem pariatur consequatur eaque sunt ipsa quasi ipsam repellat totam
+        voluptate unde consectetur, delectus nobis quos in, veniam fuga!
+        Voluptatem expedita accusamus itaque suscipit voluptatum incidunt
+        laboriosam veniam quas atque magnam excepturi non animi impedit aperiam
+        culpa, harum tenetur officiis architecto. Quia itaque doloremque optio
+        cumque maiores, ipsa laudantium at, necessitatibus officiis aspernatur
+        deleniti perspiciatis culpa reiciendis atque quas ipsum fuga animi quos
+        excepturi est consequatur incidunt facere. Laboriosam saepe reiciendis
+        labore ipsam corrupti? Aliquam et soluta exercitationem fugit. Illo
+        voluptatum odit molestias quia sequi similique cupiditate quas alias,
+        assumenda nostrum possimus, quae harum quod. Assumenda voluptate, labore
+        libero corporis deserunt saepe ab sequi praesentium optio recusandae
+        porro laboriosam delectus itaque debitis necessitatibus.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis a
+        reprehenderit odit nihil aperiam dicta? Similique quis eligendi numquam
+        quam. Illo, ullam, dolorum necessitatibus rerum et, aspernatur ipsum
+        consectetur ipsam recusandae quasi facere iure fugiat nam maiores!
+        Accusantium alias perferendis necessitatibus odit amet exercitationem
+        reprehenderit dolor laudantium sint? Rerum officia ad voluptate
+        cupiditate perspiciatis explicabo perferendis ipsa mollitia placeat ab
+        veniam labore quas similique officiis possimus, porro veritatis culpa
+        dolores quis magnam quae praesentium saepe temporibus blanditiis! Non
+        dolorum beatae, tempora quo sit ratione velit ullam laudantium nulla
+        minus adipisci praesentium corrupti? Dolore consequuntur qui eligendi
+        iste eaque, laboriosam deserunt?
+      </p>
+    </div>
+  );
+};
+
+export default About;
